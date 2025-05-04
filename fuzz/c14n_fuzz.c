a2a61c0b8d711890e3ff21b98a713c1c1c457c06
lrlt11 <lunaralet@hotmail.be>
2025-05-04 15:59:10 +0000

    changes in harness

--- fuzz/c14n_fuzz.c
+++ fuzz/c14n_fuzz.c
@@ -5,6 +5,8 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     if(!data || size == 0) {
@@ -14,24 +16,24 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     // convert the data input into a xml c string
     char *xml_string = malloc(size + 1); // + 1 for null terminator
     if (!xml_string) {
-        return 0; 
+        return 0;
     }
-    memcpy(xml_string, data, size); 
-    xml_string[size] = '\0'; 
+    memcpy(xml_string, data, size);
+    xml_string[size] = '\0';
 
     // parse the xml string
-    xmlDocPtr xml_doc = xmlReadMemory(xml_string, size, "fuzz.xml", NULL, 0); 
+    xmlDocPtr xml_doc = xmlReadMemory(xml_string, size, "fuzz.xml", NULL, 0);
     if (!xml_doc) {
         free(xml_string);
-        return 0; 
+        return 0;
     }
-    xmlChar *canonicalized_xml = NULL; 
+    xmlChar *canonicalized_xml = NULL;
     xmlC14NDocDumpMemory(xml_doc, NULL, 0, NULL, 0, &canonicalized_xml);
 
     if (canonicalized_xml) xmlFree(canonicalized_xml);
 
-    free(xml_string); 
+    free(xml_string);
     xmlFreeDoc(xml_doc);
-    xmlCleanupParser(); 
+    xmlCleanupParser();
     return 0;
 }
